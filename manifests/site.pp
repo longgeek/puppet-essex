20cd3b71ebc6c5f54bd158481970f1f6fd30e9c1
longgeek <longgeek@gmail.com>
2012-10-26 15:41:54 +0800

    Space->Tab

--- manifests/site.pp
+++ manifests/site.pp
@@ -20,19 +20,19 @@ node 'www.longgeek.com' {
 ##模块执行的顺序依赖关系,例如$keystone_require = ‘mysql::comand'就是说agent执行keystone这个模块的时候需要先执行完mysql::command这个类
 ##默认模块执行顺序: mysql -> keystone -> auth_file -> glance -> rabbitmq -> nova_control&nova_control_compute -> nova_compute -> dashboard -> swift_proxy -> swift_storage
 
-$keystone_require				= 'mysql::command'
-$auth_file_require				= 'keystone::command'
-$glance_require					= 'auth_file'
-$rabbitmq_require				= 'glance::command'
-$nova_control_compute_require	= 'rabbitmq'
-$dashboard_require				= 'nova_control_compute::command'
-$swift_proxy_require			= 'dashboard::command'
-$swift_storage_require			= 'swift_proxy::command'
+$keystone_require               = 'mysql::command'
+$auth_file_require              = 'keystone::command'
+$glance_require                 = 'auth_file'
+$rabbitmq_require               = 'glance::command'
+$nova_control_compute_require   = 'rabbitmq'
+$dashboard_require              = 'nova_control_compute::command'
+$swift_proxy_require            = 'dashboard::command'
+$swift_storage_require          = 'swift_proxy::command'
 
 ## Base -------------------------------------------------------------------------------------------------------------
 $command_path                   = '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/bin/bash'
 $verbose                        = 'False'
-$all_on_one_ip					= '172.16.0.11'
+$all_on_one_ip                  = '172.16.0.11'
 
 ## Mysql -------------------------------------------------------------------------------------------------------------
 $mysql_root_password            = 'mysql'
@@ -69,18 +69,18 @@ $multi_host                     = 'false'
 $libvirt_type                   = 'kvm'
 $nova_control_host              = $all_on_one_ip
 $nova_compute_host              = $all_on_one_ip
-$novnc_base_url_ip				= $all_on_one_ip
+$novnc_base_url_ip              = $all_on_one_ip
 $network_size                   = '256'
 
 ## Swift -------------------------------------------------------------------------------------------------------------
-$proxy_workers					= '2'
-$storage_workers				= '2'
-$storage_numbers				= '1'
-$storage_node_ip				= "[\"$all_on_one_ip\"]"
-$storage_disk_part				= '["sda5"]'  #根据实际情况来指定分区，确保有剩余空间
-$storage_base_dir				= '/swift-storage/'
-$ring_part_power				= '18'
+$proxy_workers                  = '2'
+$storage_workers                = '2'
+$storage_numbers                = '1'
+$storage_node_ip                = "[\"$all_on_one_ip\"]"
+$storage_disk_part              = '["sda5"]'  #根据实际情况来指定分区，确保有剩余空间
+$storage_base_dir               = '/swift-storage/'
+$ring_part_power                = '18'
 $ring_replicas                  = '1'
-$ring_min_part_hours			= '1'
-$swift_hash_suffix				= 'http://www.longgeek.com'
-$memcached_host					= $all_on_one_ip
+$ring_min_part_hours            = '1'
+$swift_hash_suffix              = 'http://www.longgeek.com'
+$memcached_host                 = $all_on_one_ip
